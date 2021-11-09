ed7e844aafdd2de3776ea08743fb026c187d7f00
Andy Chan <andy.chan9930@gmail.com>
2021-11-09 14:12:15 -0500

    commit tree 957147b9391cb4c504f61f9590a8526a2814bbcd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const scuberHq = 42
+let blocksTraveled;
+let feetTraveled;
+let fare;
+
+function distanceFromHqInBlocks (pickup) {
+    blocksTraveled = scuberHq > pickup ? scuberHq - pickup : pickup - scuberHq
+    return blocksTraveled
+}
+
+function distanceFromHqInFeet (value) {
+    const test = distanceFromHqInBlocks(value)
+    return feetTraveled =  test * 264
+}
+
+function distanceTravelledInFeet (starting, ending){
+    feetTraveled = starting > ending ? (starting - ending)*264 : (ending - starting)*264 
+    return feetTraveled
+}
+
+function calculatesFarePrice (start,end) {
+    if (distanceTravelledInFeet(start,end) < 400) {
+        return fare = 0
+    } else if (distanceTravelledInFeet(start,end) < 2000) {
+        return fare = (distanceTravelledInFeet(start,end) - 400) * 0.02
+    } else if (distanceTravelledInFeet(start,end) < 2500) {
+        return fare = 25
+    } else {
+        return fare = "cannot travel that far"
+    }
+}
+
+console.log(calculatesFarePrice(15, 20))
+
+
+
+
